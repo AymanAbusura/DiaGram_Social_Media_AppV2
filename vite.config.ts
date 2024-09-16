afebf655eea11005f42d1aee090e34a3a491f2e8
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-09-16 09:49:18 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { createFilter, defineConfig } from "vite"
 import svgr from 'vite-plugin-svgr'
 
+
+function removeUseClient() {
+  const filter = createFilter(/.*\.(js|ts|jsx|tsx)$/);
+
+  return {
+    name: 'remove-use-client',
+
+    transform(code: string, id: string) {
+      if (!filter(id)) {
+        return null;
+      }
+
+      const newCode = code.replace(/['"]use client['"];\s*/g, '');
+
+      return  { code: newCode, map: null };
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), removeUseClient()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@src": path.resolve(__dirname, "./src"),
+      "@ui": path.resolve(__dirname, '../../packages/ui/src')
     },
   },
 })
